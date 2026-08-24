665b5c94129e125e42f5c29c8446b4046f4e3257
Dino René Caballero Marquez <dino.caballero@gmail.com>
2026-08-24 00:51:07 -0300

    Add generic bridge for external FZ plugins

---
+++ libretro/fz_plugin_bridge.h
@@ -0,0 +1,32 @@
+#ifndef FZ_PLUGIN_BRIDGE_H
+#define FZ_PLUGIN_BRIDGE_H
+
+#include <stdint.h>
+#include "../mupen64plus-core/src/api/m64p_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FZ_PLUGIN_BRIDGE_MAGIC 0x465A4252u /* 'FZBR' */
+#define FZ_PLUGIN_BRIDGE_ABI   1u
+
+typedef m64p_function (*fz_plugin_get_proc_t)(const char *name);
+
+struct fz_plugin_core_bridge
+{
+   uint32_t magic;
+   uint32_t abi_version;
+   fz_plugin_get_proc_t get_proc;
+};
+
+/* Internal to ReARMed. The returned pointer is passed as CoreLibHandle to
+ * unmodified-style Mupen64Plus FZ plugins. It is not exported by libretro/link.T.
+ */
+const struct fz_plugin_core_bridge *fz_plugin_bridge_get(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FZ_PLUGIN_BRIDGE_H */
